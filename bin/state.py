d8d07b2554d5f3d8ae54acb3ca94ecdc285e7aa6
Junjun Mao <junjun.mao@gmail.com>
2019-12-20 12:03:26 -0500

    update

--- bin/state.py
+++ bin/state.py
@@ -4,7 +4,7 @@ import re
 from energy import *
 
 if __name__ == "__main__":
-    ph = 7.0
+    ph = 0.0
     eh = 0.0
 
     if len(sys.argv) > 1:
@@ -13,9 +13,22 @@ if __name__ == "__main__":
         cutoff = PW_PRINT_CUT
 
     lines = open("states").readlines()
+    line = lines.pop(0)
+    fields = line.strip().split(",")
+    mc_parm = {}
+    for field in fields:
+        key, value = field.split("=")
+        mc_parm[key.strip()] = float(value)
+
+    T = mc_parm["T"]
+    ph = mc_parm["ph"]
+    eh = mc_parm["eh"]
+
+
+
     for line in lines:
         state = [int(x) for x in re.findall(r"[\w']+", line)]
-        analyze_state_energy(state, ph=7.0, eh=0.0, cutoff=cutoff)
+        analyze_state_energy(state, ph=ph, eh=eh, cutoff=cutoff)
 
 #    for conf in head3lst:
 #        conf.printme()
